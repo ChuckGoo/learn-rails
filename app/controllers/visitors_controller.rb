75b8c6adbb9dd78a7f21970cc128119ac4e3dc43
Chuk Ezekiel <chukezekielu@gmail.com>
2018-08-03 23:07:00 +0200

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,5 +2,7 @@
 class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
+		flash.now[:notice] = 'Nnoo!'
+		flash.now[:alert]  = 'Ubochi omumu m nabia  mgbe nadighi anya.'
 	end
 end
