88597686ebcdad35ed9e2705514373499ae526c6
Chuk Ezekiel <chukezekielu@gmail.com>
2018-08-03 16:29:54 +0200

    Dynamic home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
-Rails.application.routes.draw do
+#   Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#  end
+#  NOTE: First and 4th line about were just commented by me.
+
+LearnRails::Application.routes.draw do
+  root to: 'visitors#new'
 end
+
+
+
